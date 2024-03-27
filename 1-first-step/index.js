f0780bf8719dd2aa08bef3b59237767bb57b9f5c
cyberfolk <andrea1995ferrari@gmail.com>
2024-03-27 22:51:11 +0100

    [ADD] module http v2

--- 1-first-step/index.js
+++ 1-first-step/index.js
@@ -87,72 +87,6 @@ console.log("A-SYNC [7] -> [Flusso normal] -> FINE");
 */
 
 /****************************************************************************************************************
-*  MODULO HTTP - V1
-* --------------------------------------------------------------------------------------------------------------*
-* res.write() si usa per scrivere 1+ più pezzi di dati nella risposta senza chiuderla.                          *
-* res.end segnala la fine della risposta e può, può anche inviare un ultimo pezzo di dati prima di chiuderla.   *
-* Si usa res.write più volte se occorre inviare dati in più passaggi. Dopo aver inviato tutti i dati necessari, *
-* Si usa res.end per chiudere la risposta. Se occorre inviare un singolo messaggio o pezzo di dati, si puo      *
-* semplicemente passarlo come parametro a res.end senza la necessità di chiamare prima res.write.               *
-*****************************************************************************************************************/
-const http = require('http')
-const host = "localhost"
-const PORT = process.env.PORT || 3000;  // Cerco la variabile d'ambiente process.env.PORT, se non la trovo userò 3000.
-// Per settare process.env.PORT -> Da terminale: $ export PORT=5000
-// Per vedere  process.env.PORT -> Da terminale: $ echo $PORT
-const server = http.createServer((req, res) => {
-    if (req.url === "/favicon.ico") {
-        res.writeHead(204); // 204 No Content
-        return res.end();
-    }else{
-        console.log("Ricevuto richiesta: ", req.method, req.url);
-    }
-    switch (req.url) {
-        case "/":
-        case "/home":
-            res.write("<h1>Home</h1>Benvenuto sul sito");
-            res.end();
-            break;
-        case "/andrea":
-            res.write("<h1>Andrea</h1>");
-            res.end();
-            break;
-        case "/luca":
-            res.setHeader("Content-Type", "text/plain; charset=UTF-8");
-            res.write("<h1>Luca</h1>");
-            res.write("<p>Qui uso il Content-Type: text/plain</p>");
-            res.end();
-            break;
-        case "/marco":
-            res.setHeader("Content-Type", "text/html; charset=UTF-8");
-            res.write("<h1>Luca</h1>");
-            res.write("<p>qui esplicito che uso il Content-Type: text/plain</p>");
-            res.write("<p>Anche se non è necessario</p>");
-            res.end();
-            break;
-        case "/matteo":
-            const body = "<h1>Matteo</h1><p>Questa e' la stringa che contiene il body della risposa effettuata con res.writeHead(...)</p>"
-            res.writeHead(200, {
-                "Content-Type": "text/html",
-                "Content-Length": Buffer.byteLength(body),
-            });
-            res.end(body);
-            break;
-        default:
-            res.writeHead(404); 
-            res.write(`<h1>Errore</h1><p>Torna alla <a href='/'>home</a> perche' la pagina non esiste</p>`);
-            res.end();
-            break;
-    }
-});
-server.listen(PORT, host, () => {
-    console.log(`Server in ascolto sulla porta ${PORT}`);
-}); 
-server.on('error', (err) => {
-    console.log(err);
-})
-
-/****************************************************************************************************************
 *  NODEMON
 * ---------------------------------------------------------------------------------------------------------------*
 * $ npm install nodemon -D --> Installa nodemon tra le devDependencies                                           *
@@ -241,3 +175,109 @@ const {URL} = url
 const urlObj = new URL(indirizzo)
 console.log(urlObj)
 console.log(url.parse(indirizzo)) */
+
+/****************************************************************************************************************
+*  MODULO HTTP - switch
+* --------------------------------------------------------------------------------------------------------------*
+* res.write() si usa per scrivere 1+ più pezzi di dati nella risposta senza chiuderla.                          *
+* res.end segnala la fine della risposta e può, può anche inviare un ultimo pezzo di dati prima di chiuderla.   *
+* Si usa res.write più volte se occorre inviare dati in più passaggi. Dopo aver inviato tutti i dati necessari, *
+* Si usa res.end per chiudere la risposta. Se occorre inviare un singolo messaggio o pezzo di dati, si puo      *
+* semplicemente passarlo come parametro a res.end senza la necessità di chiamare prima res.write.               *
+*****************************************************************************************************************/
+/*
+const http = require('http')
+const host = "localhost"
+const PORT = process.env.PORT || 3000;  // Cerco la variabile d'ambiente process.env.PORT, se non la trovo userò 3000.
+// Per settare process.env.PORT -> Da terminale: $ export PORT=5000
+// Per vedere  process.env.PORT -> Da terminale: $ echo $PORT
+const server = http.createServer((req, res) => {
+    if (req.url === "/favicon.ico") {
+        res.writeHead(204); // 204 No Content
+        return res.end();
+    }else{
+        console.log("Ricevuto richiesta: ", req.method, req.url);
+    }
+    switch (req.url) {
+        case "/":
+        case "/home":
+            res.write("<h1>Home</h1>Benvenuto sul sito");
+            res.end();
+            break;
+        case "/andrea":
+            res.write("<h1>Andrea</h1>");
+            res.end();
+            break;
+        case "/luca":
+            res.setHeader("Content-Type", "text/plain; charset=UTF-8");
+            res.write("<h1>Luca</h1>");
+            res.write("<p>Qui uso il Content-Type: text/plain</p>");
+            res.end();
+            break;
+        case "/marco":
+            res.setHeader("Content-Type", "text/html; charset=UTF-8");
+            res.write("<h1>Luca</h1>");
+            res.write("<p>qui esplicito che uso il Content-Type: text/plain</p>");
+            res.write("<p>Anche se non è necessario</p>");
+            res.end();
+            break;
+        case "/matteo":
+            const body = "<h1>Matteo</h1><p>Questa e' la stringa che contiene il body della risposa effettuata con res.writeHead(...)</p>"
+            res.writeHead(200, {
+                "Content-Type": "text/html",
+                "Content-Length": Buffer.byteLength(body),
+            });
+            res.end(body);
+            break;
+        default:
+            res.writeHead(404); 
+            res.write(`<h1>Errore</h1><p>Torna alla <a href='/'>home</a> perche' la pagina non esiste</p>`);
+            res.end();
+            break;
+    }
+});
+server.listen(PORT, host, () => {
+    console.log(`Server in ascolto sulla porta ${PORT}`);
+}); 
+server.on('error', (err) => {
+    console.log(err);
+})
+*/
+
+/****************************************************************************************************************
+*  MODULO HTTP - obj routing + readStream
+*****************************************************************************************************************/
+const http = require('http')
+const fs = require('fs')
+const host = "localhost"
+const PORT = process.env.PORT || 3000;  // Cerco la variabile d'ambiente process.env.PORT, se non la trovo userò 3000.
+const server = http.createServer((req, res) => {
+    let htmlfile = ""
+    const routing = {
+        '/': 'home.html',
+        '/home': 'home.html',
+        '/contatti': 'contatti.html'
+    }
+    res.setHeader("Content-Type", "text/html; charset=UTF-8");
+    rende(res, routing[req.url]);
+});
+
+function rende(res, htmlfile) {
+    fs.stat(`./${htmlfile}`, (err, stats) => {
+        if (stats) { 
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "text/html; charset=UTF-8");
+            fs.createReadStream(htmlfile).pipe(res);
+        }else{
+            res.statusCode = 404;
+            res.end("Risorsa Non trovata");
+        }
+    });
+}
+
+server.listen(PORT, host, () => {
+    console.log(`Server in ascolto sulla porta ${PORT}`);
+}); 
+server.on('error', (err) => {
+    console.log(err);
+})
