c186c0f234c1aaced744c791c52f684df633ccd2
cyberfolk <andrea1995ferrari@gmail.com>
2024-03-30 00:51:59 +0100

    [UPD] 7-dotenv

--- 7-dotenv/index.js
+++ 7-dotenv/index.js
@@ -1,30 +1,14 @@
 /****************************************************************************************************************
 *  DOTENV
 * --------------------------------------------------------------------------------------------------------------*
-* In base a come eseguo il programma da terminale posso cambiare le variabili d'ambiente                        *
-* Con: $ NODE_ENV=production npm start --> app.get('env') = 'production'                                        *
-* Con: $ npm start ----------------------> app.get('env') = 'development'                                       *
-* E in base a app.get('env') posso eseguire due file di convfigurazione differente                              *
-*****************************************************************************************************************/
-
-const morgan = require('morgan');
-const express = require('express');
-const app = express();
+* Con: $ NODE_ENV=production npm start --> process.env.NODE_ENV = 'production'  --> userò .env.production       *
+* Con: $ NODE_ENV=production npm start --> process.env.NODE_ENV = 'development' --> userò .env.development      *
+****************************************************************************************************************/
 
 require('dotenv').config({
-    path: `.env.${ app.get('env') }`
+    path: `.env.${process.env.NODE_ENV}`
 });
 
-console.log(app.get('env'));
-console.log(`.env.${ app.get('env') }`);
+console.log(process.env.NODE_ENV);
+console.log(`.env.${process.env.NODE_ENV}`);
 console.log(process.env.APP_PWD);
-
-if (app.get('env') === 'development') {
-    app.use(morgan('tiny'));
-}
-
-app.get('/', (req, res) => {
-    res.send('Home page\n ');    
-})
-
-app.listen(3000);
