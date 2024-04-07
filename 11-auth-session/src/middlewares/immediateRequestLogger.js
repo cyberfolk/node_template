277a4d820ad73afe8348b79edcc4262ec6a3c4c5
cyberfolk <andrea1995ferrari@gmail.com>
2024-04-08 00:44:06 +0200

    [WIP] 11-auth-session

---
+++ 11-auth-session/src/middlewares/immediateRequestLogger.js
@@ -0,0 +1,16 @@
+/******************************************************************************************************************
+ *  MIDDLEWARE = IMMEDIATE LOG REQUESTS                                                                           *
+ * ---------------------------------------------------------------------------------------------------------------*
+ * ES: 2024-04-02T21:47:23.040Z - GET /api/people
+ *****************************************************************************************************************/
+
+/**
+ * Middleware per loggare immediatamente i dettagli delle richieste in arrivo.
+ * Stampa il timestamp corrente, il metodo HTTP e il percorso della richiesta.
+ */
+const immediateRequestLogger = (req, res, next) => {
+	console.log(`${new Date().toISOString()} - ${req.method} ${req.path}`);
+	next(); // Passa il controllo al prossimo middleware o route handler
+};
+
+module.exports = immediateRequestLogger;
