475ed13747118fe343e411a9fd699b4d333426e0
cyberfolk <andrea1995ferrari@gmail.com>
2024-04-13 16:40:48 +0200

    [ADD] route demoSession and middleware cookieSession

--- 11-auth-session/index.js
+++ 11-auth-session/index.js
@@ -9,20 +9,18 @@ require("dotenv").config({
 	path: `.env.${process.env.NODE_ENV}`,
 });
 
-const MongoStore = require('connect-mongo');
-const session = require('express-session');
-const { v4: uuidv4 } = require('uuid');
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
 
 const immediateRequestLogger = require("./src/middlewares/immediateRequestLogger");
+const demoSessionRouter = require("./src/routes/demoSession");
 const handleExitSignal = require("./src/utils/handleExitSignal");
+const cookieSession = require("./src/middlewares/cookieSession");
 const mongoClient = require("./src/connections/mongoClient");
 const errorCheck = require("./src/middlewares/errorCheck");
 
 const PORT = process.env.PORT || 3000; // Cerco la variabile d'ambiente process.env.PORT, se non la trovo userò 3000.
-const SESSION_SECRET_KEY = process.env.SESSION_SECRET_KEY
 
 // Eventhanlder
 process.on("SIGINT", () => handleExitSignal("SIGINT")); // SIGINT indica una terminazione da parte dell'utente.
@@ -33,15 +31,10 @@ app.use(immediateRequestLogger);
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true })); // Consente di elaborare dati nel req.body
-app.use(session({
-	secret: SESSION_SECRET_KEY,
-	resave: false,
-	saveUninitialized: true,
-	cookie: { secure: false, httpOnly: true, maxAge: 3600000 },
-	genid: () => uuidv4(),
-	store: new MongoStore({ client: mongoClient, dbName: 'blog' })
-}))
+app.use(cookieSession(mongoClient))
+app.use("/", demoSessionRouter);
 app.use(errorCheck); // Controllo errori da posizionare per ultimo.
+
 
 async function run() {
 	try {
---
+++ 11-auth-session/src/middlewares/cookieSession.js
@@ -0,0 +1,25 @@
+/******************************************************************************************************************
+ *  COOKIE SESSION                                                                                   *
+ *****************************************************************************************************************/
+
+const MongoStore = require('connect-mongo');
+const session = require('express-session');
+const { v4: uuidv4 } = require('uuid');	// Recupero la funzione v4 e la assegno alla variabile uuidv4
+
+
+const secretKey = encodeURIComponent(process.env.SESSION_SECRET_KEY)
+const dbName = encodeURIComponent(process.env.DB_NAME);
+
+
+function cookieSession(client) {
+    return session({
+        secret: secretKey,
+        resave: false,
+        saveUninitialized: true,
+        cookie: { secure: false, httpOnly: true, maxAge: 3600000 },
+        genid: () => uuidv4(),
+        store: new MongoStore({ client: client, dbName: dbName })
+    })
+}
+
+module.exports = cookieSession;
